6c14dbf0c0809e73f001a7c662308a9dbe7fff7f
Volker Mische <volker.mische@gmail.com>
2020-07-19 23:13:56 +0200

    Remove commented out code

--- main.cpp
+++ main.cpp
@@ -1,6 +1,4 @@
 #include <QtQuick>
-//#include <QtGui>
-//#include <QtPlugin>
 
 Q_IMPORT_PLUGIN(QsgEpaperPlugin)
 
@@ -8,9 +6,6 @@ int main(int argc, char *argv[])
 {
     qputenv("QMLSCENE_DEVICE", "epaper");
     qputenv("QT_QPA_PLATFORM", "epaper:enable_fonts");
-    //qputenv("QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS", "rotate=180");
-//    qputenv("QT_QPA_EVDEV_TOUCHSCREEN_PARAMETERS", "/dev/input/event1");
-//    qputenv("QT_QPA_EGLFS_NO_LIBINPUT", "1");
 
     QGuiApplication app(argc, argv);
     QQuickView view;
